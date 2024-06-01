4545ec998a2a75df7c3cd013b5b8a6d0f5744e6b
Madimetja Terence Chuene <155132652+Takashi-91@users.noreply.github.com>
2024-06-01 11:43:49 +0000

    Initial commit

---
+++ app/src/main/java/com/example/ikamvayouth/ConfirmationScreen.kt
@@ -0,0 +1,11 @@
+package com.example.ikamvayouth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConfirmationScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirmation_screen)
+    }
+}
